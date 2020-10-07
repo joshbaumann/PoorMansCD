d714ca85796d52ed6005e8dcb5d4269b76ecd65a
Joshua Baumann <joshua.baumann@harmoney.co.nz>
2020-10-08 10:59:06 +1300

    Changes

--- apps/app1/src/app.service.ts
+++ apps/app1/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from App1!';
+    return 'Hello from App1 IT WORKSSS!!!';
   }
 }
